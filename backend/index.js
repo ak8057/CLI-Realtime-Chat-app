0a1f294a2cc1d31c5d39daf8a672486f2404a1ec
Abhay Kumar <abhay292005@gmail.com>
2024-09-24 18:43:39 +0530

    Frontend setup done

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,32 @@
-const express=require('express')
-
-const data=require('./data/data.js')
-
-const app=express()
-const port=3000
-
-app.get('/' ,(req,res) => {
-    res.send('Hello World')
-})
-
-app.get('/api/auth' , (req,res)=>{
-    res.send(`${data}`)
-})
+const express = require("express");
+const data = require("./data/data.js");
+const chats = require("./data/data.js");
+const dotenv = require("dotenv");
+dotenv.config();
 
 
+const app = express();
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(data);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  // console.log(req.params.id);
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
 
 
 
-app.listen(port, ()=> {
-    console.log(`Server is running on port ${port}`)
-})
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
