ef8e6f1370c35ffe50ad9e9bca8cb7d2d5eaece7
Abhay Kumar <abhay292005@gmail.com>
2024-10-11 12:43:22 +0530

    minor fixes

--- backend/index.js
+++ backend/index.js
@@ -62,7 +62,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(notFound);
 app.use(errorHandler);
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 const server = app.listen(
   port,
