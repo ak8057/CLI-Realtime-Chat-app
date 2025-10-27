4628e8561947c2d2f712e96bb410106ee87d5ab1
Abhay Kumar <abhay292005@gmail.com>
2025-10-27 17:33:54 +0530

    minor bug fixes

--- frontend/src/components/MyChats.js
+++ frontend/src/components/MyChats.js
@@ -70,6 +70,7 @@ const MyChats = ({ fetchAgain }) => {
         <GroupChatModal>
           <Button
             display="flex"
+            color="black"
             fontSize={{ base: "17px", md: "10px", lg: "17px" }}
             rightIcon={<AddIcon />}
           >
